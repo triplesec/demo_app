86dbd4164399974f402b34a16c4ee28bad32232e
triplesec <teoprojects@gmail.com>
2010-11-29 19:39:55 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '05cf5188ac2c099c5381d9cf1223a80c9260154fa2498db858157556507419ce914d80d291f5f2399afb9fdbb3dfa6edcf7661dde69e2d41ea69962a25db3758'
